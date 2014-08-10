01a5fe3946d480979816b8ca8b92be3f5c7f5bc6
CAMOKPYT <camokpyt@gmail.com>
2014-08-10 23:52:06 +0400

    Metaprogramming mafacer. Do u speak it?

--- database.rb
+++ database.rb
@@ -1,2 +1,14 @@
 require 'sequel'
-
+class DatabaseConnector
+  Sequel.extension :inflector
+  class << self
+    attr_accessor :data_source
+  end
+  @data_source = 'sqlite://toster.db'
+    db = Sequel.connect(DatabaseConnector.data_source)
+    tables = %w(answers answers_comments comments questions tags users)
+    tables.each do |table|
+      raise "Table '#{table}' does not exists or you have no permission to query" unless db.table_exists? table
+      Kernel.const_set(table.classify, Class.new(Sequel::Model(db[table.to_sym])))
+    end
+end
---
+++ user_page.rb
@@ -0,0 +1,28 @@
+class UserPage
+  @users_url = 'http://toster.ru/users/main?page='
+  class << self
+    attr_accessor :users_url
+  end
+  def initialize(page_id,user_agent=nil,cookie=nil)
+    @page_id = page_id
+    @user_agent = user_agent
+    @cookie = cookie
+    @page = Nokogiri::HTML(open(UserPage.users_url+@page_id.to_s, {'User-Agent' => @user_agent, 'Cookie' => @cookie}) )
+    @users = []
+  end
+
+
+  def get_users
+    @page.css('div.left_column div.inner div.users_list_item').each do |el|
+      @users << User.new(
+          nick      =  el.css('div.login').text,
+          rating    =  el.css('div.rating a b').text,
+          answers   =  el.css('div.answers_count a b').text,
+          questions =  el.css('div.questions_count a b').text,
+          name      =  el.css('div.fullname a').text
+      )
+    end
+    @users
+  end
+
+end
